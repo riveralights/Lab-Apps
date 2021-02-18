21b8ebd1a79527357ccffce9819100209c50df8b
riveralights <riveralights@gmail.com>
2021-02-18 19:53:53 +0700

    Membuat Migration, Model dan Controller Laboratory

---
+++ database/migrations/2021_02_18_124741_create_laboratories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaboratoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laboratories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('code', 7);
+            $table->string('author', 75);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laboratories');
+    }
+}
